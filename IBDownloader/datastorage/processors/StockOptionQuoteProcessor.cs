2a72f9da422b410feb3a7da6946c2aed29137ab8
Jason H <jason.hillier@paviasystems.com>
2018-03-21 18:06:37 -0700

    wip

--- IBDownloader/datastorage/processors/StockOptionQuoteProcessor.cs
+++ IBDownloader/datastorage/processors/StockOptionQuoteProcessor.cs
@@ -5,6 +5,8 @@ using IBDownloader.messages;
 using IBApi;
 using IBDownloader.Tasks;
 using System.Collections;
+using IBDownloader.Managers;
+using System.Linq;
 
 namespace IBDownloader.DataStorage.Processors
 {
@@ -12,14 +14,20 @@ namespace IBDownloader.DataStorage.Processors
 	{
 		public override bool CheckIfSupported(TaskResultData taskResult)
 		{
-			if (taskResult.Data is IEnumerable<HistoricalDataMessage>)
-			{
-				return true;
-			}
-			else
+			if (!(taskResult.Data is IEnumerable<HistoricalDataMessage>))
 			{
 				return false;
 			}
+
+			if (taskResult.Instruction.SecType == SecurityType.STK.ToString() ||
+					taskResult.Instruction.SecType == SecurityType.IND.ToString() ||
+					taskResult.Instruction.SecType == SecurityType.FUT.ToString() ||
+					taskResult.Instruction.SecType == SecurityType.FUT.ToString())
+			{
+				return true;
+			}
+
+			return false;
 		}
 
 		public void PreConvert(BaseDataStorage storage, TaskResultData taskResult)
@@ -39,29 +47,47 @@ namespace IBDownloader.DataStorage.Processors
 			//fallback
 			if (taskResult.Instruction.contract.SecType == "OPT")
 				return base.Convert(taskResult);
-			
-			
-			//else we are updating a stock quote into prior downloaded options
-			List<OptionsQuoteProcessor.OptionQuote> optionQuotes = (List<OptionsQuoteProcessor.OptionQuote>)taskResult.Metadata["OptionQuotes"];
-			var stockOptionQuotes = optionQuotes.ConvertAll<StockOptionQuote>((q)=>
+
+			List<HistoricalDataMessage> stockQuotes = (List<HistoricalDataMessage>)taskResult.Data;
+			//TODO: we might want to reconcile this
+			//index stock quotes by date
+			Dictionary<DateTime, HistoricalDataMessage> stockQuotesIndexed = new Dictionary<DateTime, HistoricalDataMessage>();
+			stockQuotes.All((q) =>
 			{
-				var iQuote =  q as StockOptionQuote;
-				iQuote.StockQuote = ;
+				var quoteDate = DateTime.Parse(q.Date);
+				stockQuotesIndexed[quoteDate] = q;
+				return true;
+			});
+
+			//else we are updating a stock quote into prior downloaded options
+			List<OptionQuote> optionQuotes = (List<OptionQuote>)taskResult.Metadata["OptionQuotes"];
+			List<StockOptionQuote> stockOptionQuotes = optionQuotes.ConvertAll((q) =>
+			{
+				var iQuote = q as StockOptionQuote;
+				if (stockQuotesIndexed.ContainsKey(iQuote.date))
+				{
+					iQuote.StockQuote = stockQuotesIndexed[iQuote.date];
+				}
+				else
+				{
+					this.LogWarn("Skipping option quote {0} because no underlying quote found for {1}", iQuote.symbol, iQuote.date);
+				}
+
 				return iQuote;
 			});
             
             return stockOptionQuotes;
         }
         
-        public class StockOptionQuote : OptionsQuoteProcessor.OptionQuote
+        public class StockOptionQuote : OptionQuote
 		{
-            public StockOptionQuote(Contract OptionContract, Contract Underlying, HistoricalDataMessage OptionQuote, HistogramDataMessage StockQuote)
+            public StockOptionQuote(Contract OptionContract, Contract Underlying, HistoricalDataMessage OptionQuote, HistoricalDataMessage StockQuote)
                 : base(OptionContract, Underlying, OptionQuote)
 			{
 				this.StockQuote = StockQuote;
             }
 
-			public HistogramDataMessage StockQuote {get;set;}
+			public HistoricalDataMessage StockQuote {get;set;}
         }
     }
 }
